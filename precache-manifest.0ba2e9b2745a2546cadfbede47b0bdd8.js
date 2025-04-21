d6d188397fe65238d14276f52bf0572f3647035d
blazecodeprakhar <prakharyadav079@gmail.com>
2025-04-21 19:12:27 +0530

    Updates

---
+++ precache-manifest.0ba2e9b2745a2546cadfbede47b0bdd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ed00f2791de038f5e798a35bb543fe",
+    "url": "/blazeforge/index.html"
+  },
+  {
+    "revision": "59a1126b4083b7164cc8",
+    "url": "/blazeforge/static/css/main.01d1884e.chunk.css"
+  },
+  {
+    "revision": "b4cee1b876d6c26a8389",
+    "url": "/blazeforge/static/js/2.6f58f2f5.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/blazeforge/static/js/2.6f58f2f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59a1126b4083b7164cc8",
+    "url": "/blazeforge/static/js/main.8ed31e5d.chunk.js"
+  },
+  {
+    "revision": "cb96dda613cd3d13c739",
+    "url": "/blazeforge/static/js/runtime-main.da5bd5ed.js"
+  }
+]);
